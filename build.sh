ad9fd40ad264f9022f3a23c7c24f7c60397cee4e
Alexander K <alexkuz@gmail.com>
2024-04-15 12:13:18 +0200

    add builder

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+TEXT="Alexander Kuznetsov Frontend developer CV"
+
+ENC_TEXT=$(echo "$TEXT" | jq -sRr @uri)
+
+rm -rf alexkuz.me/fonts
+
+npx webfont-dl "https://fonts.googleapis.com/css2?family=Jost:wght@300;400&display=swap&text=$ENC_TEXT" \
+	--css-rel "fonts" \
+	-o alexkuz.me/fonts/font.css
+
+npx html-inline-external --src index.html --dest alexkuz.me/index.html 
